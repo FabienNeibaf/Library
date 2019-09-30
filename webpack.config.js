ecf43adaa3c3cf1dc42b6d029a275c9dd235f6b5
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-09-30 18:17:28 +0300

    Set environment

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist")
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                {
+                  pragma: "Penina.createElement",
+                  pragmaFrag: "Penina.Fragment"
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};
